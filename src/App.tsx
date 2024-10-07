f707a13e0e899e811a7d6193ba322c94aad436c8
ElieldsonKervem <kervem.elieldson@academico.ifrn.edu.br>
2024-10-07 20:42:16 -0300

    project created

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+
+import styled from 'styled-components'
+
+const Tittle = styled.h1 `
+  
+ font-size: 3rem;
+  color: #333;
+  
+`
+
+
+function App() {
+
+
+  return (
+    <>
+      <div className='App'>
+        <Tittle>
+          React Bank
+        </Tittle>
+      </div>
+    </>
+  )
+}
+
+export default App
